73c83bd168f04287387ad2322d70fbf1923a0d64
franck cuny <franck@lumberjaph.net>
2010-09-15 11:33:59 +0200

    POD for mw useragent

--- lib/Net/HTTP/Spore/Middleware/UserAgent.pm
+++ lib/Net/HTTP/Spore/Middleware/UserAgent.pm
@@ -1,5 +1,7 @@
 package Net::HTTP::Spore::Middleware::UserAgent;
 
+# ABSTRACT: middleware to change the user-agent value
+
 use Moose;
 extends qw/Net::HTTP::Spore::Middleware/;
 
@@ -11,5 +13,13 @@ sub call {
     $req->header('User-Agent' => $self->useragent);
 }
 
-
 1;
+
+=head1 SYNOPSIS
+
+    my $client = Net::HTTP::Spore->new_from_spec('twitter.json');
+    $client->enable('UserAgent', useragent => 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0b4) Gecko/20100818 Firefox/4.0b4');
+
+=head1 DESCRIPTION
+
+Net::HTTP::Spore::Middleware::UserAgent change the default value of the useragent.
