954976ce006715b249088bfc2314ab2afb571a1e
franck cuny <franck@lumberjaph.net>
2010-09-15 15:21:24 +0200

    remove useless files

---
+++ lib/Net/HTTP/Spore/Middleware/Mock.pm
@@ -0,0 +1,38 @@
+package Net::HTTP::Spore::Middleware::Mock;
+
+# ABSTRACT: Simple Mocker for Spore middlewares
+
+use Moose;
+extends 'Net::HTTP::Spore::Middleware';
+
+has tests => ( isa => 'HashRef', is => 'ro', required => 1 );
+
+sub call {
+    my ( $self, $req ) = @_;
+
+    foreach my $r ( keys %{ $self->tests } ) {
+        next unless $r eq $req->path;
+        my $res = $self->tests->{$r}->($req);
+        return $res if defined $res;
+    }
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    my $mock_server = {
+        '/path/i/want/to/match' => sub {
+            my $req = shift;
+            ...
+            $req->new_response(200, ['Content-Type' => 'text/plain'], 'ok');
+        }
+    };
+
+    my $client = Net::HTTP::Spore->new_from_spec('spec.json');
+    $client->enable('Mock', tests => $mock_server);
+    my $res = $client->my_rest_method();
+    is $res->status, 200;
+    is $res->body, 'ok';
+
+=head1 DESCRIPTION
