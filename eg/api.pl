dba1ee2b1252ad4ef8a64622daf6c2dd5ddb2557
franck cuny <franck@lumberjaph.net>
2010-10-17 21:40:28 +0200

    simplify github example

--- eg/api.pl
+++ eg/api.pl
@@ -4,21 +4,10 @@ use 5.010;
 
 use Net::HTTP::Spore;
 
-my $username = shift;
-my $token = shift;
-
 my $api = Net::HTTP::Spore->new_from_spec(shift);
 
 $api->enable('Net::HTTP::Spore::Middleware::Format::JSON');
 
-$api->enable(
-    'Net::HTTP::Spore::Middleware::Auth::Basic',
-    username => $username,
-    password => $token,
-);
+my ( $content, $result ) = $api->user_information( format => 'json', username => 'franckcuny' );
 
-my ( $content, $result ) =
-  $api->user_information( format => 'json', username => 'franckcuny' );
-
-use YAML::Syck;
-warn Dump $content;
+say "name => ". $content->{body}->{user}->{name};
