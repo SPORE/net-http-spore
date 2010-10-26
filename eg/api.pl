8b841d433f33fb9d72aefff025a01cdfb202ea29
franck cuny <franck@lumberjaph.net>
2010-10-26 17:35:23 +0200

    fix method name

--- eg/api.pl
+++ eg/api.pl
@@ -8,6 +8,6 @@ my $api = Net::HTTP::Spore->new_from_spec(shift);
 
 $api->enable('Net::HTTP::Spore::Middleware::Format::JSON');
 
-my ( $content, $result ) = $api->user_information( format => 'json', username => 'franckcuny' );
+my ( $content, $result ) = $api->get_info( format => 'json', username => 'franckcuny' );
 
 say "name => ". $content->{body}->{user}->{name};
