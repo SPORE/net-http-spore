ec616a2639d7c8773c1c7bc2aad59f2c67bcef4f
franck cuny <franck@lumberjaph.net>
2011-02-21 18:23:35 +0100

    basic code to log

--- lib/Net/HTTP/Spore/Role/Debug.pm
+++ lib/Net/HTTP/Spore/Role/Debug.pm
@@ -1,21 +1,50 @@
 package Net::HTTP::Spore::Role::Debug;
 
+use IO::File;
 use Moose::Role;
 
 has trace => (
     is      => 'rw',
-    isa     => 'Bool',
+    isa     => 'Int',
     lazy    => 1,
-    default => sub { $ENV{SPORE_TRACE} ? 1 : 0; }
+    default => sub {
+        my $self      = shift;
+        my $trace_env = $ENV{SPORE_TRACE};
+        my @stack = caller; use YAML; warn Dump \@stack;
+        my ($fh, $level);
+        if ( defined($trace_env) && ( $trace_env =~ /(\d)=(.+)$/ ) ) {
+            $level = $1;
+            $fh = IO::File->new( $2, 'w' )
+              or die("Cannot open trace file $1");
+        }
+        else {
+            $level = $trace_env;
+            $fh = IO::File->new('>&STDERR')
+              or die('Duplication of STDERR for debug output failed (perhaps your STDERR is closed?)');
+        }
+        $fh->autoflush();
+        $self->_trace_fh($fh);
+        return $level;
+    }
 );
 
-has handle => (
-    is => 'rw', isa => 'Object',
+has _trace_fh => (
+    is      => 'rw',
+    isa     => 'GLOB',
 );
 
 sub _trace_msg {
+    my $self     = shift;
+    my $template = shift;
+    return unless $self->trace;
+    my $fh = $self->_trace_fh();
+    print $fh (sprintf( $template, @_ )."\n");
+}
+
+sub _trace_verbose {
     my $self = shift;
-    print STDOUT $_[0]."\n" if $self->trace;
+    return unless $self->trace && $self->trace > 1;
+    $self->_trace_msg(@_);
 }
 
 1;
