e90e9a78948d8f8417987b688bc99e7200513866
straux <stephane.raux@linkfluence.net>
2011-11-03 12:34:07 +0100

    Add utf8 param in Format::JSON middleware

--- lib/Net/HTTP/Spore/Middleware/Format/JSON.pm
+++ lib/Net/HTTP/Spore/Middleware/Format/JSON.pm
@@ -6,11 +6,12 @@ use JSON;
 use Moose;
 extends 'Net::HTTP::Spore::Middleware::Format';
 
+has utf8 => ( is => 'ro', isa => 'Bool', default => 1 );
 has _json_parser => (
     is      => 'rw',
     isa     => 'JSON',
     lazy    => 1,
-    default => sub { JSON->new->utf8(1)->allow_nonref },
+    default => sub { JSON->new->utf8(shift->utf8)->allow_nonref },
 );
 
 sub encode       { $_[0]->_json_parser->encode( $_[1] ); }
@@ -24,7 +25,10 @@ sub content_type { ( 'Content-Type' => 'application/json;' ) }
 
     my $client = Net::HTTP::Spore->new_from_spec('twitter.json');
     $client->enable('Format::JSON');
-
+    
+    # alternative to disable utf8 auto-encoding
+    $client->enable('Format::JSON', utf8 => 0);
+    
 =head1 DESCRIPTION
 
 Net::HTTP::Spore::Middleware::Format::JSON is a simple middleware to handle the JSON format. It will set the appropriate B<Accept> header in your request. If the request method is PUT or POST, the B<Content-Type> header will also be set to JSON.
